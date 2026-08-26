4049d7f69732047478c7294e97bf6491447b21f4
Fliimedia <info@flii.nl>
2026-08-26 21:07:00 +0200

    Path-routing: /p/internship/international rendert de internship form

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import FliiSite from "./FliiSite.jsx";
+import InternshipInternational from "./InternshipInternational.jsx";
+
+const path = window.location.pathname.replace(/\/+$/, "");
+const isInternship = path === "/p/internship/international";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <FliiSite />
+    {isInternship ? <InternshipInternational /> : <FliiSite />}
   </React.StrictMode>
 );
