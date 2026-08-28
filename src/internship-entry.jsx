0437340f3e17185725fe3a74660ebd34b8d1be0f
Fliimedia <info@flii.nl>
2026-08-28 05:21:39 +0200

    Multi-page entry-bestand niet meer nodig

--- src/internship-entry.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import InternshipInternational from "./InternshipInternational.jsx";
-
-createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <InternshipInternational />
-  </React.StrictMode>
-);
