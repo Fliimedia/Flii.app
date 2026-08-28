65e6cf029bcbab265c2a5736b798b04915d4acdd
Fliimedia <info@flii.nl>
2026-08-28 05:07:59 +0200

    Eigen JS-entry voor het stageformulier

---
+++ src/internship-entry.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import InternshipInternational from "./InternshipInternational.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <InternshipInternational />
+  </React.StrictMode>
+);
