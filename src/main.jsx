5329d4721976f4dcecc69e8e820a7e480b0b469f
Fliimedia <info@flii.nl>
2026-08-28 05:08:02 +0200

    main.jsx terug naar origineel (formulier heeft nu eigen pagina)

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import FliiSite from "./FliiSite.jsx";
-import InternshipInternational from "./InternshipInternational.jsx";
-
-const strip = (s) => s.replace(/\/+$/, "");
-const TARGET = "/p/internship/international";
-const path = strip(window.location.pathname);
-const hash = strip(window.location.hash.replace(/^#/, ""));
-const isInternship = path === TARGET || hash === TARGET;
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {isInternship ? <InternshipInternational /> : <FliiSite />}
+    <FliiSite />
   </React.StrictMode>
 );
